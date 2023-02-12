41365c05b86c37ce6e70d14d30af325a3e9246a5
ShokhruhKholboyev <shoxruh9902@gmail.com>
2023-02-12 10:15:12 +0500

    BROKERS: Insert Guest

---
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.cs
@@ -0,0 +1,10 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    { }
+}
