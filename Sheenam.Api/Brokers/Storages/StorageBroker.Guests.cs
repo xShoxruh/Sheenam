e44e36969e110a2ade08fc25316941eac4e415f2
ShokhruhKholboyev <shoxruh9902@gmail.com>
2023-02-17 21:46:58 +0500

    CODE RUB: Remove Empty Line

--- Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
@@ -5,7 +5,6 @@
 
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Sheenam.Api.Models.Foundations.Guests;
 
 namespace Sheenam.Api.Brokers.Storages
