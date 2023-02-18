1e6aba9a28088f465a97033072509eaa3e6dafd3
ShokhruhKholboyev <shoxruh9902@gmail.com>
2023-02-18 06:49:23 +0500

    CODE RUB: Rename Function

--- Sheenam.Api/Brokers/Storages/IStorageBroker.Guests.cs
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.Guests.cs
@@ -12,6 +12,6 @@ namespace Sheenam.Api.Brokers.Storages
     public partial interface IStorageBroker
     {
         ValueTask<Guest> InsertGuestAsync(Guest guest);
-        IQueryable<Guest> SelectAllStudents();
+        IQueryable<Guest> SelectAllGuests();
     }
 }
