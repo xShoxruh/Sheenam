8c92cb865fdbe58a0b2846f5161c9f87b6fbcfd6
ShokhruhKholboyev <shoxruh9902@gmail.com>
2023-02-20 12:15:59 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfIsInvalidAndLogItAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,53 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+
+using FluentAssertions;
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Models.Foundations.Guests.Exceptions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIsInvalidAndLogItAsync()
+        {
+            //given
+            var invalidGuestId = Guid.Empty;
+
+            var invalidGuestException = new InvalidGuestException();
+
+            invalidGuestException.AddData(
+                key: nameof(Guest.Id),
+                values: "Id is required");
+
+            var expectedGuestValdiationException = new
+                GuestValidationException(invalidGuestException);
+
+            //when
+            ValueTask<Guest> retrieveGuestByIdAsync = 
+                this.guestService.RetrieveGuestByIdAsync(invalidGuestId);
+
+            GuestValidationException actualGuestValidationException =
+                await Assert.ThrowsAsync<GuestValidationException>(
+                    retrieveGuestByIdAsync.AsTask);
+
+            //them
+
+            actualGuestValidationException.Should().BeEquivalentTo(expectedGuestValdiationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGuestValdiationException))), Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuestByIdAsync(It.IsAny<Guid>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
