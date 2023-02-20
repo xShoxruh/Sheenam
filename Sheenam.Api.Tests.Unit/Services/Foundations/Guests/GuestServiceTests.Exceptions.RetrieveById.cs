fc37c6bae38b224b5b29e8e17912c86e43410ba3
ShokhruhKholboyev <shoxruh9902@gmail.com>
2023-02-20 14:16:20 +0500

    ShouldThrowExceptionOnRetrieveByIdIfSqlErrorOccurred -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,49 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests.Exceptions;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowExceptionOnRetrieveByIdIfSqlErrorOccurred()
+        {
+            // given
+            Guid randomId = Guid.NewGuid();
+            SqlException sqlException = GetSqlError();
+
+            var failedGuestStorageException =
+                new FailedGuestStorageException(sqlException);
+
+            var guestDependencyException =
+                new GuestDependencyException(failedGuestStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGuestByIdAsync(randomId)).ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Guest> RetrieveByIdTask =
+                this.guestService.RetrieveGuestByIdAsync(randomId);
+
+            // then
+            await Assert.ThrowsAsync<GuestDependencyException>(() =>
+                RetrieveByIdTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuestByIdAsync(It.IsAny<Guid>()), Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+            broker.LogCritical(guestDependencyException), Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
